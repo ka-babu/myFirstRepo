72442accae7d908fd50795e3ae4c41cc79b2a607
“[ka <[kunj2kab@gmail.com]>
2019-11-19 06:44:17 -0800

    first file in github repo

---
+++ first-git-file.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	printf("This is my first git file\n");
+
+	printf("Trying to track the changes made\n");
+
+	printf("This statement is included from branch3\n");
+	return 0;
+}
